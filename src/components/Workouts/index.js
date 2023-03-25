d2810ae26495af70a4f9e4daf4df496e5063cb9a
Maverick <Mav33987@gmail.com>
2023-03-24 19:30:23 -0500

    fixed input having duplicate values on creation

--- src/components/Workouts/index.js
+++ src/components/Workouts/index.js
@@ -1,95 +1,80 @@
 import React from 'react';
 import { useState } from "react";
-import { useFormik } from 'formik'
+
 
 
 function Workouts() {
 
-    const [inputs, setInputs] = useState([{key: '', value: ''}]);
-    const [exercise, setExercise] = useState([''])
-    const [weight, setWeight] = useState([''])
-    const [reps, setReps] = useState([''])
-    /* let newInputs = [...inputs] */;
-  
+    const [inputs, setInputs] = useState([{ key: '', value: '' }]);
+    const [exercise, setExercise] = useState({
+        exercise:'',
+        weight:'',
+        reps:''
+    })
+    
 
 
 
-    const addHandler = ()=>{
-        
-  
-      /*   newInputs.push({key: '', value: ''}); */
+
+
+    const addHandler = () => {
+
+
+       
         setInputs((newInputs) => [...newInputs, ''])
     }
 
-    const deleteHandler = (key)=>{
-        const newInputs = inputs.filter((input,index) => index !== key);
+    const deleteHandler = (key) => {
+        const newInputs = inputs.filter((input, index) => index !== key);
         setInputs(newInputs);
     }
 
-    const submitHandler = (event)=>{
+ /*    const submitHandler = (event) => {
         let newExercise = [...exercise]
         event.preventDefault();
-       newExercise.push(exercise)
+        newExercise.push(exercise)
         setExercise(newExercise);
         setWeight('');
         setReps('');
-        console.log(exercise, weight, reps)
-    }
-
-    function form() {
         
-        inputs.map((input, key) =>(
-            <form key={key} onSubmit={submitHandler}>
-            <input placeholder='exercise' /* value={exercise} */ /* onChange={event => {
-                setExercise(event.target.value)
-            }} */ {...formik.getFieldProps('exercise')[key]}></input>
-            <input placeholder='weight' /* value={weight} *//*  onChange={event => {
-                setWeight(event.target.value)
-            }} */ {...formik.getFieldProps('weight')[key]}></input>
-            <input placeholder='reps' /* value={reps} */ /* onChange={event => {
-                setReps(event.target.value)
-            }} */ {...formik.getFieldProps('reps')[key]}></input>
-            <button onClick={() => deleteHandler(key)}>Delete</button>
-            <button type='submit'>Submit</button>
-            </form>
-            
-    ))
-    }
+    } */
 
-    const formik = useFormik({
-        initialValues: {
-          exercise: [],
-          weight:[],
-          reps: []
-        }
-      }); 
 
-    
+
+  
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setExercise({
+          ...exercise,
+          [e.target.name]: value
+        });
+     
+      }
+
+      console.log(exercise)
+
+
+
 
     return (
         <div>
-                <h2>Fitness Tracker</h2>
-                
-                {inputs.map((input, key) =>(
-                        <form key={key} onSubmit={submitHandler}>
-                        <input placeholder='exercise' /* value={exercise} */ /* onChange={event => {
-                            setExercise(event.target.value)
-                        }} */ {...formik.getFieldProps('exercise')[key]}></input>
-                        <input placeholder='weight' /* value={weight} *//*  onChange={event => {
-                            setWeight(event.target.value)
-                        }} */ {...formik.getFieldProps('weight')[key]}></input>
-                        <input placeholder='reps' /* value={reps} */ /* onChange={event => {
-                            setReps(event.target.value)
-                        }} */ {...formik.getFieldProps('reps')[key]}></input>
-                        <button onClick={() => deleteHandler(key)}>Delete</button>
-                        <button type='submit'>Submit</button>
-                        </form>
-                        
-                ))}
-           
-        <button onClick={addHandler}>Add Exercise</button>
-       
-        
+            <h2>Fitness Tracker</h2>
+
+            {inputs.map((input, key) => (
+                <form onChange={(e) => handleChange(e)} key={key} >
+                    <input placeholder='exercise'  name='exercise' ></input>
+                    <input placeholder='weight' name='weight'  ></input>
+                    <input placeholder='reps' name='reps'   ></input>
+                    <button onClick={() => deleteHandler(key)}>Delete</button>
+                    <button type='submit'>Submit</button>
+                </form>
+
+            ))}
+
+            <button onClick={addHandler}>Add Exercise</button>
+
+
         </div>
     );
 }
